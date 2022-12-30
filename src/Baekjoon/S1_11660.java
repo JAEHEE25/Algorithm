5a9f798091ae90d0f4e9987e0445af09dfa7a491
JAEHEEE <y2hjjh@naver.com>
2022-12-31 00:44:55 +0900

    12.30

--- src/Baekjoon/S1_11660.java
+++ src/Baekjoon/S1_11660.java
@@ -1,7 +1,59 @@
 package Baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class S1_11660 {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+
+    //n, m 입력
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    ArrayList<int[]> sumList = new ArrayList<>();
+    int[] sum = new int[n+1];
+
+    //행렬 입력
+    int[][] matrix = new int[n][n];
+
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < n; j++) {
+        matrix[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    for (int[] arr : matrix) {
+      //합배열 만들기
+      sum = new int[n+1];
+      for (int i = 1; i <= n; i++) {
+        sum[i] = sum[i-1] + arr[i-1];
+      }
+      sumList.add(sum);
+    }
+
+    //구해야하는 합배열
+    for (int i = 0; i < m; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x1 = Integer.parseInt(st.nextToken());
+      int y1 = Integer.parseInt(st.nextToken());
+      int x2 = Integer.parseInt(st.nextToken());
+      int y2 = Integer.parseInt(st.nextToken());
+      int result = 0;
+
+      for (int x = x1-1; x <= x2-1; x++) {
+        int[] rowSum = sumList.get(x);
+        result += rowSum[y2] - rowSum[y1-1];
+      }
+      sb.append(result + "\n");
+    }
+    System.out.print(sb);
 
   }
 }
--- src/Baekjoon/S4_1337.java
+++ src/Baekjoon/S4_1337.java
@@ -1,7 +1,44 @@
 package Baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class S4_1337 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    ArrayList<Integer> list = new ArrayList<>();
+
+    for (int i = 0; i < n; i++) {
+      list.add(Integer.parseInt(br.readLine()));
+    }
+
+    Collections.sort(list);
+
+    int[] cnt = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 1; j < 5; j++) {
+        if (!list.contains(list.get(i) + j)) {
+          cnt[i]++;
+        };
+      }
+    }
+
+//    Arrays.sort(cnt);
+//    System.out.println(cnt[0]);
+
+    int min = cnt[0];
+    for (int c : cnt) {
+      if (c <= min) {
+        min = c;
+      }
+    }
+    System.out.println(min);
   }
 }
