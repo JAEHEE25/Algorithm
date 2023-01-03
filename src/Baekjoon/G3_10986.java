6d5ef1c4ea96eecd63f34b1154166646eb9d9520
JAEHEEE <y2hjjh@naver.com>
2023-01-03 22:30:29 +0900

    01.03

--- src/Baekjoon/G3_10986.java
+++ src/Baekjoon/G3_10986.java
@@ -4,20 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
-//하는중
 public class G3_10986 {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    StringBuilder sb = new StringBuilder();
 
-    //n, m, cnt
+    //n, m, ans
     int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
-    int cnt = 0;
+    long ans = 0;
 
     //n개의 수 배열
     ArrayList<Integer> input = new ArrayList<>();
@@ -27,36 +29,58 @@ public class G3_10986 {
       input.add(Integer.parseInt(st.nextToken()));
     }
 
-    //구간합 만들기
-//    while (n > 0) {
-//      int[] sum = new int[n + 1];
-//
-//      for (int i = 1; i <= n; i++) {
-//        sum[i] = sum[i - 1] + input.get(i - 1);
-//      }
-//
-//      cnt += (int) Arrays.stream(sum).filter(x -> x % m == 0).count() - 1;
-//      input.remove(0);
-//      n--;
-//    }
-
-    int[] sum = new int[n + 1];
-
+    //누적합 배열
+    long[] sum = new long[n+1];
     for (int i = 1; i <= n; i++) {
-      sum[i] = sum[i - 1] + input.get(i - 1);
+      sum[i] = sum[i - 1] + input.get(i-1);
     }
 
-    System.out.println(Arrays.toString(sum));
-    int[] result = new int[n + 1];
+    //나머지 배열
+    ArrayList<Long> rest = new ArrayList<>();
+    for (int i = 1; i <= n; i++) {
+      rest.add(sum[i] % m);
 
-    for (int i = 1; i <= n; i ++) {
-      result[i-1] = sum[i] - sum[1];
-      System.out.println(result[i-1]);
-      System.out.println(Arrays.toString(result));
-//      sum = result;
     }
 
-    sb.append(cnt);
-    System.out.print(sb);
+    //나머지 배열에 0이 있을 경우 개수 1 더하기
+    for (long r: rest) {
+      if (r == 0) {
+        ans += 1;
+      }
+    }
+
+    //나머지 배열에서 개수 구하기
+    //나머지 배열 중복제거
+    List<Long> done = rest.stream().distinct().collect(Collectors.toList());
+    for (long d : done) {
+      long cnt = Collections.frequency(rest, d);  //배열에서 개수 구하기
+      ans += (cnt * (cnt - 1) / 2);
+    }
+    System.out.println(ans);
+
+//    Scanner sc = new Scanner(System.in);
+//    int n = sc.nextInt();
+//    int m = sc.nextInt();
+//    long[] S = new long[n];
+//    long[] C = new long[m];
+//    long answer = 0;
+//    //바로 입력받아서 합 배열 만들기
+//    S[0] = sc.nextInt();
+//    for (int i = 1; i <= n; i++) {
+//      S[i] = S[i - 1] + sc.nextInt();
+//    }
+//    for (int i = 0; i < n; i++) {
+//      int remainder = (int) (S[i] % m);
+//      //구간합 자체가 0일때 정답에 더하기
+//      if (remainder == 0) answer++;
+//      //나머지에 해당하는 인덱스 개수 카운팅
+//      C[remainder]++;
+//    }
+//    for (int i = 0; i < m; i++) {
+//      if (C[i] > 1) {
+//        answer += C[i] * (C[i] - 1) / 2;
+//      }
+//    }
+//    System.out.println(answer);
   }
 }
--- src/Baekjoon/S5_2018.java
+++ src/Baekjoon/S5_2018.java
@@ -3,6 +3,7 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class S5_2018 {
@@ -12,9 +13,51 @@ public class S5_2018 {
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     int n = Integer.parseInt(st.nextToken());
+    int start = 1;
+    int end = 2;
+    int sum;
+    int answer = 1;
 
+    while (start < n / 2 + 1) {
+      sum = (start + end) * (end - start + 1) / 2;
 
+      if (sum < n) {
+        end++;
+      }
+      else if (sum == n) {
+        answer++;
+        start++;
+        end = start + 1;
+      }
+      else {
+        start++;
+        end = start + 1;
+      }
+    }
 
+    System.out.println(answer);
+
+//    Scanner sc = new Scanner(System.in);
+//    int n = sc.nextInt();
+//    int count = 1;
+//    int start_index = 1;
+//    int end_index = 1;
+//    int sum = 1;
+//
+//    while (end_index != n) {
+//      if (sum == n) {
+//        count++;
+//        end_index++;
+//        sum += end_index;
+//      } else if (sum > n) {
+//        sum -= start_index;
+//        start_index++;
+//      } else {
+//        end_index++;
+//        sum += end_index;
+//      }
+//    }
+//    System.out.println(count);
   }
 
 }
