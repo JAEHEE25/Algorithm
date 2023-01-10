1174ee7915e83432dbc8df6309b2919e6e85b1ca
JAEHEE <y2hjjh@naver.com>
2023-01-10 15:26:45 +0900

    [Silver IV] Title: 카드2, Time: 268 ms, Memory: 48996 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    Queue<Integer> card = new LinkedList<>();
+
+    for (int i = 1; i <= n; i++) {
+      card.add(i);
+    }
+
+    for (int i = 0; i < n-1; i++) {
+      card.poll();
+      card.add(card.peek());
+      card.poll();
+    }
+
+    System.out.println(card.peek());
+
+  }
+}
