0e1032daf00b24609d253a5f99186230b6384ffb
JAEHEEE <y2hjjh@naver.com>
2023-01-12 11:07:00 +0900

    01.11

--- src/Baekjoon/S1_11286.java
+++ src/Baekjoon/S1_11286.java
@@ -15,52 +15,41 @@ public class S1_11286 {
     StringBuilder sb = new StringBuilder();
     int n = Integer.parseInt(br.readLine());
 
-//    PriorityQueue<Long> positive = new PriorityQueue<>();
-//    PriorityQueue<Long> negative = new PriorityQueue<>();
-
-    ArrayList<Long> list = new ArrayList<>();
+    PriorityQueue<Long> positive = new PriorityQueue<>();
+    PriorityQueue<Long> negative = new PriorityQueue<>(Collections.reverseOrder());
 
     //x가 0이면 -> 절댓값 가장 작은 값(여러개면 음수) 출력 후 제거
-    //절댓값 기준 정렬하고 -3 -1 0 3 5, 앞뒤 두개가 절댓값 같으면 앞에거 출력
-    //안 같으면 둘 중에 절댓값 작은거 출력
+    //p에 양수 추가, n에 음수 추가
 
+    //안 같으면 둘 중에 절댓값 작은거 출력
 
     for (int i = 0; i < n; i++) {
       long x = Long.parseLong(br.readLine());
 
-      if (x == 0) {
-        if (list.size() == 1) {
+      if (x == 0) {  //x가 0일 때
+        if (positive.isEmpty() && negative.isEmpty()) {  //둘 다 비어있으면 0 출력
           sb.append("0" + "\n");
         } else {
-          int idx = list.indexOf(0L);
-//          System.out.println(list);
-
-          int next = idx + 1;
-          int prev = idx - 1;
-          if (idx == 0) {
-          sb.append(list.get(next) + "\n");
-//            System.out.println("1 " + list.get(next));
-            list.remove(list.get(next));
-          } else if (idx == list.size() - 1) {
-          sb.append(list.get(prev) + "\n");
-//            System.out.println("2 " + list.get(prev));
-            list.remove(list.get(prev));
-          } else {
-            if (Math.abs(list.get(prev)) <= Math.abs(list.get(next))) {
-//              System.out.println("3 " + list.get(prev));
-              sb.append(list.get(prev) + "\n");
-              list.remove(list.get(prev));
-            }
-            else {
-//              System.out.println("4 " + list.get(next));
-              sb.append(list.get(next) + "\n");
-              list.remove(list.get(next));
+          if (positive.isEmpty()) sb.append(negative.poll() + "\n");
+          else if (negative.isEmpty()) sb.append(positive.poll() + "\n");
+          else {
+            long absP = Math.abs(positive.peek());
+            long absN = Math.abs(negative.peek());
+            if (absP == absN) {  //각 맨 앞 비교해서 절댓값 같으면 n 맨앞 출력
+              sb.append(negative.poll() + "\n");
+            } else {
+              if (absP < absN) sb.append(positive.poll() + "\n");
+              else sb.append(negative.poll() + "\n");
             }
           }
+
         }
-      } else {  //x가 0이 아니면 값 추가
-        list.add(x);
+
+      } else {  //x가 0이 아닐 때
+        if (x < 0) negative.add(x);  //음수
+        else positive.add(x);  //양수
       }
+
     }
 
     System.out.print(sb);
