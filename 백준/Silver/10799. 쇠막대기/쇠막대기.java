6de6e6929e02bf055d7f1d655b6a5c0e161c6363
정재희 <y2hjjh@naver.com>
2023-07-19 00:53:17 +0900

    [Silver II] Title: 쇠막대기, Time: 188 ms, Memory: 16336 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,39 +1,31 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Stack;
- 
+
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        // TODO Auto-generated method stub
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String input = bf.readLine();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int answer = 0; //쇠막대기 개수
+
         Stack<Character> stack = new Stack<>();
- 
-        int result = 0;
+
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == '(') { // 열린 괄호면 스택에 추가.
-                stack.push('(');
-                continue;
-            }
-            if (input.charAt(i) == ')') { // 닫힌 괄호일 경우,
-                stack.pop(); // 일단 stack에서 pop을 실행.
- 
-                if (input.charAt(i - 1) == '(') { // 그 전 괄호가 열린 괄호면 레이저를 의미.
-                    result += stack.size(); // 현재 stack의 사이즈만큼 더해 줌.
-                } else { // 그 전 괄호가 닫힌 괄호면 레이저가 아님.
-                    result++; // 단순히 1을 더해 줌.
+            char letter = input.charAt(i);
+            if (letter == '(') { //여는 괄호일 경우 스택에 push
+                stack.push(letter);
+            } else { //닫는 괄호일 경우
+                stack.pop();
+                if (input.charAt(i - 1) == '(') { //바로 전 문자가 여는 괄호일 경우
+                    answer += stack.size();
+                } else { //쇠막대기가 끝남
+                    answer++;
                 }
             }
         }
- 
-        bw.write(result + "\n");
-        bw.flush();
-        bf.close();
-        bw.close();
-    }
- 
-}
+        System.out.println(answer);
+    }//main
+
+}//class
