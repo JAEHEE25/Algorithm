ac0345828630946c5ec7ee2c596a5e87975bc86e
정재희 <y2hjjh@naver.com>
2023-08-10 23:35:51 +0900

    [Silver III] Title: N과 M (3), Time: 564 ms, Memory: 71676 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static boolean[] visit;
+    static int[] answer;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        visit = new boolean[N+1];
+        answer = new int[M];
+
+        recur(0);
+        System.out.print(sb);
+    }//main
+
+    static void recur(int depth) {
+        if (depth == M) {
+            for (int num : answer) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            answer[depth] = i;
+            recur(depth + 1);
+        }
+    }//recur
+}//class
