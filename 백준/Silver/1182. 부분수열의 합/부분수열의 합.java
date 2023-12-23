6a80ffcf92af4a824bfaa308c7498cb84fa19e28
정재희 <y2hjjh@naver.com>
2023-12-23 23:37:47 +0900

    [Silver II] Title: 부분수열의 합, Time: 216 ms, Memory: 17768 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 class Main {
     static int[] num;
-    private static int N;
-    private static int S;
+    private static int N; // 정수의 개수
+    private static int S; // 정수의 합
     private static int answer = 0;
 
     public static void main(String[] args) {
@@ -16,6 +16,8 @@ class Main {
         }
         dfs(0, 0);
         if (S == 0) System.out.println(answer - 1); 
+        //S 0일때가 전체 합이 0 일때랑 겹치기 때문에 -1 을 해주어야 합니다.
+        //위의 사진에 공집합이 0이라고 볼 수 있습니다
          else  System.out.println(answer);
 
     }
@@ -25,6 +27,7 @@ class Main {
             if (sum == S) answer++;
             return;
         }
+        //tree를 생각해서 직접 그려보면 이해하기가 쉽습니다.
         dfs(depth + 1, sum + num[depth]);
         dfs(depth + 1, sum); 
     }
