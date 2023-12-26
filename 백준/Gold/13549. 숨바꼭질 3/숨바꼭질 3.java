ec044ce34c768415b18c3412f26c9ca6304263b5
정재희 <y2hjjh@naver.com>
2023-12-26 18:09:07 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 324 ms, Memory: 90168 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,45 +1,72 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
- 
-public class Main {    
- 
-    static int min = Integer.MAX_VALUE;
-    static int n, k;
-    static boolean[] visited;
+
+public class Main {
+    static int subin;
+    static int sister;
+    static Queue<Place> queue;
+    static boolean[] visit;
+    static int answer = Integer.MAX_VALUE;
     static int max = 100000;
-    
-    public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[] args) throws IOException {
+//        String pack = Main.class.getPackage().toString().split(" ")[1];
+//        String realPack = pack.split("[.]")[1] + "/" + pack.split("[.]")[2];
+//        BufferedReader br = new BufferedReader(new FileReader(realPack + "/input.txt"));
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        subin = Integer.parseInt(st.nextToken());
+//        sister = Integer.parseInt(st.nextToken());
+
+
+        Scanner sc = new Scanner(System.in);
+        subin = sc.nextInt();
+        sister = sc.nextInt();
+
+        visit = new boolean[max + 1];
+        BFS();
+        System.out.println(answer);
+
+    }//main
+
+    static void BFS() {
+        queue = new LinkedList<>();
+        queue.add(new Place(subin, 0));
         
-        n = scan.nextInt();
-        k = scan.nextInt();
-        
-        visited = new boolean[max + 1];
-        bfs();
-        System.out.println(min);
-    }
-    
-    public static void bfs() {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(n, 0));
-        
-        while(!q.isEmpty()) {
-            Node node = q.poll();
-            visited[node.x] = true;
-            if(node.x == k) min = Math.min(min, node.time);
-            
-            if(node.x * 2 <= max && visited[node.x * 2] == false) q.offer(new Node(node.x * 2, node.time));
-            if(node.x + 1 <= max && visited[node.x + 1] == false) q.offer(new Node(node.x + 1, node.time + 1));
-            if(node.x - 1 >= 0 && visited[node.x - 1] == false) q.offer(new Node(node.x - 1, node.time + 1));
+        while (!queue.isEmpty()) {
+            Place poll = queue.poll();
+            visit[poll.place] = true;
+
+            if (poll.place == sister) {
+                answer = Math.min(answer, poll.time);
+            }
+
+            int plusOne = poll.place + 1;
+            int minusOne = poll.place - 1;
+            int mulTwo = poll.place * 2;
+            check(mulTwo, poll, 0);
+            check(plusOne, poll, 1);
+            check(minusOne, poll, 1);
+
         }
     }
-    
-    public static class Node {
-        int x;
-        int time;
-        
-        public Node(int x, int time) {
-            this.x = x;
-            this.time = time;
+
+    static void check(int value, Place poll, int addTime) {
+        if (value >= 0 && value <= 100000 && !visit[value]) {
+            queue.add(new Place(value, poll.time + addTime));
         }
+    }
+}//class
+
+class Place {
+    int place;
+    int time;
+
+    Place(int place, int time) {
+        this.place = place;
+        this.time = time;
     }
 }
