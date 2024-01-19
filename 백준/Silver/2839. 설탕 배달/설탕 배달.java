670ec0f7056c3ed588df6cb09e1860cdf2c31e60
정재희 <y2hjjh@naver.com>
2024-01-20 00:11:49 +0900

    [Silver IV] Title: 설탕 배달, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,25 +2,24 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-  static int n;
-
   public static void main(String[] args) throws IOException {
     Scanner sc = new Scanner(System.in);
-    n = sc.nextInt();
+    int N = sc.nextInt();
     int cnt = 0;
 
     while (true) {
-      if (n % 5 == 0) {
-        System.out.println(n / 5 + cnt);
+      if (N % 5 == 0) {
+        cnt += N / 5;
         break;
       }
-      if (n < 0) {
-        System.out.println(-1);
-        break;
-      }
-      n -= 3;
+      N -= 3;
       cnt++;
-    }
-  }//main
 
+      if (N < 0) {
+        cnt = -1;
+        break;
+      }
+    }
+    System.out.println(cnt);
+  }//main
 }//class
