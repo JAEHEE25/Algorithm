909f44ec9a4ed5d0acdb87362a323a4f0d22e250
정재희 <y2hjjh@naver.com>
2024-02-15 20:41:43 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.15 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,50 +1,34 @@
-import java.util.*;
+import java.util.Stack;
+
 class Solution {
-    static Map<Character, Character> map = new HashMap<>();
-    static int answer = 0;
-    
-    public int solution(String s) {
-        map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
-        List<Character> sList = new LinkedList<>();
-        for (char c : s.toCharArray()) {
-            sList.add(c);
-        }
-        check(sList);
-        
-        List<Character> changed = new LinkedList<>();
-        for (int i = 1; i < sList.size(); i++) {
-            changed.add(sList.get(i));
-        }
-        changed.add(sList.get(0));
-        
-        while (!changed.equals(sList)) {
-            check(changed);
-            //회전
-            changed.add(changed.get(0));
-            changed.remove(0);
-        }
-        return answer;
-    }
-    
-    public void check(List<Character> str) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : str) {
-            if (map.keySet().contains(c)) {
-                if (stack.isEmpty()) {
-                    stack.push(c);
-                    break;
-                }
-                if (map.get(c) == stack.peek()) stack.pop(); //짝이면 pop
-                else {
-                    stack.push(c);
-                    break;
-                }
-            } else {
-                stack.push(c);
+    private final Stack<Character> stack = new Stack<>();
+
+        public int solution(String s) {
+            int answer = 0;
+            StringBuilder stringBuilder = new StringBuilder(s);
+
+            for (int i = 0; i < s.length(); i++) {
+                stringBuilder.append(stringBuilder.charAt(0));
+                stringBuilder.deleteCharAt(0);
+                if (correctParenthesis(stringBuilder.toString().toCharArray()))
+                    answer++;
             }
-        }     
-        if (stack.isEmpty()) answer++;
-    }
+            return answer;
+        }
+
+        private boolean correctParenthesis(char[] s) {
+            for (char c : s) {
+                if (!(check(c, '(', ')') && check(c, '[', ']') && check(c, '{', '}')))
+                    return false;
+            }
+            return stack.isEmpty();
+        }
+
+        private boolean check(char c, char a, char b) {
+            if (c == a)
+                stack.push(a);
+            else if (c == b)
+                if (!stack.isEmpty() && stack.peek() == a) stack.pop(); else return false;
+            return true;
+        }
 }
