d5c5c8b9c35f6103339fcee28959d6538f5354c1
정재희 <y2hjjh@naver.com>
2024-02-16 01:22:44 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,52 +1,28 @@
 import java.util.*;
+
 class Solution {
-    static int answer = 0;
     public int solution(String[] want, int[] number, String[] discount) {
-        Map<String, Integer> wantMap = new HashMap<>();
-        Map<String, Integer> disMap = new HashMap<>();
-        
-        //wantMap 만들기
-        for (int i = 0; i < want.length; i++) {
-            wantMap.put(want[i], number[i]);
-        }
-        
-        //처음 10일 확인
-        for (int i = 0; i < 10; i++) {
-            if (wantMap.keySet().contains(discount[i])) {
-                disMap.put(discount[i], disMap.getOrDefault(discount[i], 0) + 1);
+        int answer = 0;
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i=0; i<want.length; i++) {
+            while (number[i]-- > 0) {
+                list.add(want[i]);
             }
         }
-        check(wantMap, disMap);
-        
-        //다음부터 확인
-        int start = 0;
-        int end = 10;
-        while (end < discount.length) {
-            String remove = discount[start];
-            String add = discount[end];
-            if (wantMap.keySet().contains(add)) {
-                disMap.put(add, disMap.getOrDefault(add, 0) + 1);
+
+        for (int i=0; i<discount.length-list.size()+1;i ++) {
+            ArrayList<String> l = (ArrayList<String>) list.clone();
+            for (int j=i; j<i+list.size(); j++) {
+                if (l.contains(discount[j])) {
+                    l.remove(discount[j]);
+                } else {
+                    break;
+                }
             }
-            if (wantMap.keySet().contains(remove)) {
-                disMap.put(remove, disMap.getOrDefault(remove, 0) - 1);
-            }
-            check(wantMap, disMap);
-            
-            start++;
-            end++;
+            answer += l.size()==0 ? 1 : 0;
         }
-        
+
         return answer;
-    }
-    
-    public void check(Map<String, Integer> wantMap, Map<String, Integer> disMap) {
-        boolean isAnswer = true;
-        for (String item : wantMap.keySet()) {
-            if (wantMap.get(item) > disMap.getOrDefault(item, 0)) {
-                isAnswer = false;
-                break;
-            }
-        }
-        if (isAnswer) answer++;
     }
 }
