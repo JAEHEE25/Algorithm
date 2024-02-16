2b0cd5e361b54e182c2c7178823f4aa0d038d0a7
정재희 <y2hjjh@naver.com>
2024-02-16 19:39:22 +0900

    [level 2] Title: 방문 길이, Time: 4.61 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -7,7 +7,7 @@ class Solution {
     static int px = 0;
     static int py = 0;
     static List<Place> placeList = new ArrayList<>();
-    
+
     public int solution(String dirs) {
         answer = dirs.length();
         for (int i = 0; i < dirs.length(); i++) {
@@ -17,25 +17,25 @@ class Solution {
             else if (c == 'D') command = 1;
             else if (c == 'R') command = 2;
             else command = 3;
-            
+
             int nx = px + dx[command];
             int ny = py + dy[command];
-        
+
             if (nx < -5 || ny < -5 || nx > 5 || ny > 5) {
                 answer--;
                 continue; //범위 벗어나면 무시
             }
-            
+
             Place newPlace = new Place(px, py, nx, ny); //생성
             check(newPlace); //이미 가본 길인지 검사
             placeList.add(newPlace); //리스트에 추가
             px = nx; //플레이어 위치 이동
             py = ny; //플레이어 위치 이동
         }
-        
+
         return answer;
     }
-    
+
     public void check(Place newPlace) {
         //리스트에 있는지
         for (Place p : placeList) {
@@ -52,14 +52,14 @@ class Place {
     int prevY;
     int nowX;
     int nowY;
-    
+
     public Place(int prevX, int prevY, int nowX, int nowY) {
         this.prevX = prevX;
         this.prevY = prevY;
         this.nowX = nowX;
         this.nowY = nowY;
     }
-    
+
     public boolean isSame(Place a) {
         if (this.prevX == a.prevX && this.prevY == a.prevY && this.nowX == a.nowX && this.nowY == a.nowY) {
             return true;
