4fec246618ee2686e1d9b804b62c1b0a767213b4
정재희 <y2hjjh@naver.com>
2024-02-16 16:05:45 +0900

    [level 2] Title: 구명보트, Time: 8.02 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,18 +1,13 @@
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
         Arrays.sort(people);
-        
-        int idx = 0;
-        for (int i = people.length - 1; i >= idx; i--) {
-            if (people[idx] + people[i] <= limit) {
-                answer++;
-                idx++;
-            } else {
-                answer++;
-            }
-        }    
-        return answer;
+        int i = 0, j = people.length - 1;
+        for (; i < j; j--) {
+            if (people[i] + people[j] <= limit)
+                ++i;
+        }
+        return people.length - i;
     }
-} 
+}
