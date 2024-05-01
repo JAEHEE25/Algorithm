f556dd395fcfeca0383b2e5d600bc5e710988f0b
정재희 <y2hjjh@naver.com>
2024-05-02 03:02:22 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.39 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=java) 
 
 ### 성능 요약
 
-메모리: 79.5 MB, 시간: 0.14 ms
+메모리: 77.7 MB, 시간: 0.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 23:54:14
+2024년 05월 02일 03:02:20
 
 ### 문제 설명
 
--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,28 +1,13 @@
+import java.util.*;
+
 class Solution {
     public long solution(long n) {
-        String str = String.valueOf(n);
-        StringBuilder sb = new StringBuilder(str);
-        StringBuilder result = new StringBuilder();
-        
-        for (int i = 0; i < str.length(); i++) {
-            result.append(getMax(sb));
-        }
-        
-        return Long.parseLong(result.toString());
-    }
+        long answer = 0;
+        char[] arr = String.valueOf(n).toCharArray();
+        Arrays.sort(arr);
     
-    public int getMax(StringBuilder sb) {
-        int max = 0;
-        int idx = 0;
-        String sbstr = sb.toString();
-        for (int i = 0; i < sbstr.length(); i++) {
-            int num = Integer.parseInt(String.valueOf(sbstr.charAt(i)));
-            if (max < num) {
-                max = num;
-                idx = i;
-            }
-        }
-        sb.deleteCharAt(idx);
-        return max;    
+        StringBuilder sb = new StringBuilder(String.valueOf(arr));
+        answer = Long.parseLong(sb.reverse().toString());
+        return answer;
     }
 }
