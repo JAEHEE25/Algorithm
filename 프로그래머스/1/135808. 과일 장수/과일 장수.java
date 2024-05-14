1df66027af4fe07fd40a2373a8700a080715c944
정재희 <y2hjjh@naver.com>
2024-05-14 16:45:04 +0900

    [level 1] Title: 과일 장수, Time: 0.36 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,12 +1,13 @@
 import java.util.*;
- 
+
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        Arrays.sort(score);
- 
-        for(int i = score.length; i >= m; i -= m){
-            answer += m * score[i - m];
+        Arrays.sort(score); //정렬
+        
+        int startIdx = score.length % m; //버리는 사과 제외
+        for (int i = startIdx; i < score.length; i += m) {
+            answer += score[i] * m * 1; //사과 가격 계산
         }
         return answer;
     }
