5efd3a22f88c3f130236e839e0df316a1692e006
정재희 <y2hjjh@naver.com>
2024-06-05 02:33:20 +0900

    [level 2] Title: 구명보트, Time: 17.62 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,13 +1,16 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
+        int answer = 0;
         Arrays.sort(people);
-        int i = 0, j = people.length - 1;
-        for (; i < j; j--) {
-            if (people[i] + people[j] <= limit)
-                ++i;
+        
+        int l = 0;
+        for (int h = people.length - 1; h >= l; h--) {
+            if (people[l] + people[h] <= limit) { //둘이 탈 수 있으면
+                l++;
+            }
+            answer++;
         }
-        return people.length - i;
+        return answer;
     }
 }
