355d570f6cf8627e0a7f5c64de833654d4616b43
정재희 <y2hjjh@naver.com>
2024-06-05 18:55:13 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18076 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,25 +1,25 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-  public static void main(String[] args) throws IOException {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int[] dp = new int[n + 1];
-    //dp[x] = x에서 1로 만드는 최소 횟수
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1]; //dp[숫자] = 횟수의 최솟값
 
-    dp[1] = 0;
+        for (int n = 2; n <= N; n++) {
+            dp[n] = dp[n - 1] + 1; //1을 뺄 경우의 값
+            if (n % 2 == 0) { //2로 나눠지면
+                dp[n] = Math.min(dp[n], dp[n / 2] + 1);
+            }
 
-    for (int i = 2; i <= n; i++) {
-      dp[i] = dp[i - 1] + 1;
-      if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-      if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-    }
+            if (n % 3 == 0) { //3으로 나눠지면
+                dp[n] = Math.min(dp[n], dp[n / 3] + 1);
+            }
+        }
+        
+        System.out.println(dp[N]);
+    }//main
 
-
-    System.out.println(dp[n]);
-
-
-  }//main
 }//class
