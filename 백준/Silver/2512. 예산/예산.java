1c4c09795e2044a32bf92beff6d1ce9ed3170821
정재희 <y2hjjh@naver.com>
2024-06-16 02:57:05 +0900

    [Silver II] Title: 예산, Time: 156 ms, Memory: 15404 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,52 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());  //지방의 수
+		int N = Integer.parseInt(br.readLine());
+		int[] costs = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    int[] budget = new int[n];  //예산
-    int max = 0;  //최댓값
-    StringTokenizer st = new StringTokenizer(br.readLine());
+		long high = 0;
+		long total = 0;
+		for (int i = 0; i < N; i++) {
+			costs[i] = Integer.parseInt(st.nextToken());
+			high = Math.max(high, costs[i]);
+			total += costs[i];
+		}
+		long M = Long.parseLong(br.readLine());
+		long low = 0;
 
-    for (int i = 0; i < n; i++) {
-      budget[i] = Integer.parseInt(st.nextToken());
+		if (total > M) {
+			//이분 탐색
+			while (low + 1 < high) {
+				long mid = (low + high) / 2;
+				total = 0;
 
-      if (budget[i] > max) {
-        max = budget[i];
-      }
-    }
+				//total 계산
+				for (int cost : costs) {
+					total += cost > mid ? mid : cost;
+				}
 
-    int m = Integer.parseInt(br.readLine());  //총 예산
-
-    int min = 1;
-
-    while (min <= max) {
-      int upper = (min + max) / 2;
-      int sum = 0;
-
-      for (int b : budget) {
-        if (b > upper) {
-          sum += upper;
+				if (total <= M) {
+					low = mid;
+				} else {
+					high = mid;
+				}
+			}
+            System.out.println(low);
         } else {
-          sum += b;
+            System.out.println(high);
         }
-      }
 
-      if (sum > m) {
-        max = upper - 1;
-      } else {
-        min = upper + 1;
-      }
-
-    }
-
-    System.out.println(max);
-
-  }//main
-
+	}//main
 }//class
