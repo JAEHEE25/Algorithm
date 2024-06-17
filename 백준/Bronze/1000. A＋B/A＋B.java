beaa53f0dce4c359e10346c220d4cafae3a18c8e
정재희 <y2hjjh@naver.com>
2024-06-17 15:44:35 +0900

    [Bronze V] Title: A+B, Time: 184 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        System.out.println(A+B);
+    }
+}
