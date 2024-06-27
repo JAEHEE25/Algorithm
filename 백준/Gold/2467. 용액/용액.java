c2cc63d21b25dd23b0241c14c21ce71efdfe1e09
정재희 <y2hjjh@naver.com>
2024-06-27 15:48:19 +0900

    [Gold V] Title: 용액, Time: 340 ms, Memory: 28636 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -2,33 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		long[] arr = new long[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			arr[i] = Long.parseLong(st.nextToken());
-		}
-		
-		int left =0;
-		int right =n-1;
-		int ml =0, mr = 0;
-		long min = Long.MAX_VALUE;
-		while(left<right) {
-			long sum = arr[left]+arr[right];
-			if(min > Math.abs(sum)) {
-				min = Math.abs(sum);
-				ml = left; mr = right;
+        int N = Integer.parseInt(br.readLine());
+        int[] solutions = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            solutions[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int low = 0;
+        int high = N - 1;
+        long min = Long.MAX_VALUE;
+        long result;
+        long[] answer = {0, 0};
+
+        while (low < high) {
+			result = solutions[low] + solutions[high];
+
+			if (Math.abs(result) < min) {
+				min = Math.abs(result);
+				answer[0] = solutions[low];
+				answer[1] = solutions[high];
 			}
-			if(sum>=0) {
-				right--;	
-			}else {
-				left++;
-			}
-		}
-		System.out.println(arr[ml] +" "+arr[mr]);
-	}
-}
+
+			if (result < 0) low++; //합이 커져야 한다
+			else high--; //합이 작아져야 한다
+
+        }
+
+        System.out.println(answer[0] + " " + answer[1]);
+
+	}//main
+}//class
