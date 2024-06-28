c10100edafeeb10bc4cd992ff2883cf287a470ec
정재희 <y2hjjh@naver.com>
2024-06-28 16:01:29 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1212 ms, Memory: 306348 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,78 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int[] selected;
+    static boolean[] visit;
+    static boolean[] finish;
+    static int cnt;
 
-  static ArrayList<Integer> isTeam;
-  static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    StringBuilder sb = new StringBuilder();
-    int T = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder answer = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            selected = new int[N + 1];
+            visit = new boolean[N + 1]; //방문
+            finish = new boolean[N + 1]; //탐색 종료
 
-    for (int test = 0; test < T; test++) { //테스트 케이스
-      st = new StringTokenizer(br.readLine());
-      int n = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= N; i++) {
+                selected[i] = Integer.parseInt(st.nextToken());
+            }
 
-      int[] student = new int[n + 1];
-
-      isTeam = new ArrayList<>();
-      visited = new boolean[n + 1];
-
-      st = new StringTokenizer(br.readLine());
-      for (int i = 1; i <= n; i++) {  //배열에 저장
-        int x = Integer.parseInt(st.nextToken());
-        student[i] = x;
-      }
-
-//      System.out.println(student);
-
-      for (int i = 1; i <= n; i++) {
-        ArrayList<Integer> road = new ArrayList<>();  //경로 저장
-        if (visited[i]) {  //이미 방문한 학생이면 continue
-          continue;
+            cnt = 0;
+            for (int i = 1; i <= N; i++) {
+                if (!visit[i]) dfs(i); //방문하지 않은 노드에 dfs
+            }
+            answer.append(N - cnt).append("\n");
         }
-        DFS(i, student, road);
-      }
+        System.out.println(answer);
+    } //main
 
-      sb.append(n - isTeam.size() + "\n");
+    public static void dfs(int num) {
+        visit[num] = true;
+        int next = selected[num];
 
-    } //test
-
-    System.out.print(sb);
-  } //main
-
-  static void DFS(int num, int[] student, ArrayList<Integer> road) {
-
-    visited[num] = true; //방문 처리
-    road.add(num);  //경로에 num 저장
-    int next = student[num]; //num이 찍은 학생
-
-//    System.out.println("road " + road);
-    if (visited[next]) {  //이미 방문한 학생이면 return
-      if (road.contains(next)) {  //next가 road에 있으면 팀(사이클) 형성
-        //road에 있는 next의 인덱스 찾아서, 거기부터 끝까지 isTeam에 추가, 방문처리
-        int idx = road.indexOf(next);
-        for (int i = idx; i < road.size(); i++) {
-           //경로에 있는 숫자
-          isTeam.add(road.get(i));
+        if (visit[next]) { //방문함
+            if (!finish[next]) { //&&탐색 종료되지 않음 -> 사이클
+                for (int i = next; i != num; i = selected[i]) { //정점 개수 구하기 (다음 정점이 num일 때까지 순회)
+                    cnt++;
+                }
+                cnt++; //자기 자신
+            }
+        } else {
+            dfs(next);
         }
-//      System.out.println(isTeam);
-      }
-      return; //끝내기
+        finish[num] = true;
     }
-
-
-//    System.out.println(Arrays.toString(visited) + "\n" + next);
-
-    DFS(next, student, road);  //next로 DFS
-
-  } //DFS
-
-} //class
+}
