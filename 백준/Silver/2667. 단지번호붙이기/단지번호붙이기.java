82e11f3c3a27cfb89862b1be588aa7b9dd9d5cbb
정재희 <y2hjjh@naver.com>
2024-06-28 23:20:40 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,75 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-  static char[][] map;
-  static boolean[][] visited;
-  static int n;
-  static int division;
-  static int house = 0;
-  static ArrayList<Integer> houseList;
-  static int[] dx = {1, 0, 0, -1};
-  static int[] dy = {0, 1, -1, 0};
+    static int N;
+    static char[][] map;
+    static boolean[][] visit;
+    static int houseCnt;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    n = Integer.parseInt(br.readLine());
-    StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    map = new char[n][n];
-    visited = new boolean[n][n];
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        visit = new boolean[N][N];
 
-    division = 0;
-    houseList = new ArrayList<>();
-
-
-    for (int i = 0; i < n; i++) {
-      map[i] = br.readLine().toCharArray();
-    }
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (!visited[i][j] && map[i][j] == '1') {
-          DFS(i, j);
-          houseList.add(house);
-          house = 0;
-          division++;
+        for (int i = 0; i < N; i++) {
+            map[i] = br.readLine().toCharArray();
         }
-      }
+
+        int totalCnt = 0; //총 단지수
+        List<Integer> houseList = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visit[i][j] && map[i][j] == '1') {
+                    houseCnt = 0;
+                    dfs(i, j);
+                    totalCnt++;
+                    houseList.add(houseCnt);
+                }
+
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(totalCnt).append("\n");
+        
+        Collections.sort(houseList);
+        for (int h : houseList) sb.append(h).append("\n");
+
+        System.out.println(sb);
+    }//main
+
+    static void dfs(int x, int y) {
+        visit[x][y] = true;
+        houseCnt++;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N || visit[nx][ny] || map[nx][ny] == '0') continue;
+
+            dfs(nx, ny);
+        }
     }
-
-    sb.append(division + "\n");
-
-    Collections.sort(houseList);
-
-    for (int h : houseList) {
-      sb.append(h + "\n");
-    }
-
-    System.out.print(sb);
-  }//main
-
-  static void DFS(int x, int y) {
-    visited[x][y] = true;
-    house++;
-
-    for (int i = 0; i < 4; i++) {
-      int nx = x + dx[i];
-      int ny = y + dy[i];
-
-      if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
-        continue;
-      }
-
-      if (!visited[nx][ny] && map[nx][ny] == '1') {
-        DFS(nx, ny);
-      }
-    }
-
-
-
-  }//DFS
 }//class
