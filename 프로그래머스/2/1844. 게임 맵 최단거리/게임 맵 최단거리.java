479434c95f42c66e13e45b9d2dda74d624e1fb10
정재희 <y2hjjh@naver.com>
2024-11-05 14:57:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.36 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,60 +1,58 @@
 import java.util.*;
 class Solution {
+    static final int NOT_VISIT = -1;
+    static final int VISIT = 0;
+    static int[][] dist;
+    static int n;
+    static int m;
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
-    static boolean[][] visit;
-    static int N;
-    static int M;
-    static int answer = 0;
-    static boolean isAnswer = false;
     
     public int solution(int[][] maps) {
-        N = maps.length;
-        M = maps[0].length;
-        visit = new boolean[N][M];
-        BFS(0,0, maps);
-
-        if (!isAnswer) answer = -1;
-        return answer;
+        n = maps.length;
+        m = maps[0].length;
+        dist = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], NOT_VISIT);
+        }
+        
+        BFS(maps);
+        return dist[n-1][m-1] == -1 ? -1 : dist[n-1][m-1] + 1;
     }
     
-    public void BFS(int x, int y, int[][] maps) {
-        Queue<Place> queue = new LinkedList<>();
-        queue.add(new Place(x, y, 1));
+    void BFS(int[][] maps) {
+        Queue<Characters> queue = new LinkedList<>();
+        queue.add(new Characters(0, 0));
+        dist[0][0] = VISIT;
         
         while (!queue.isEmpty()) {
-            Place poll = queue.poll();
-            
-            if (poll.x == N-1 && poll.y == M-1) {
-                isAnswer = true;
-                answer = poll.cnt;
-                return;
-            }
+            Characters poll = queue.poll();
             
             for (int i = 0; i < 4; i++) {
                 int nx = poll.x + dx[i];
                 int ny = poll.y + dy[i];
                 
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visit[nx][ny] || maps[nx][ny] == 0) {
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || maps[nx][ny] == 0 || dist[nx][ny] != NOT_VISIT) {
                     continue;
                 }
                 
-                queue.add(new Place(nx, ny, poll.cnt+1));
-                visit[nx][ny] = true;
+                queue.add(new Characters(nx, ny));
+                dist[nx][ny] = dist[poll.x][poll.y] + 1;
             }
         }
-    
     }
 }
 
-class Place {
+class Characters {
     int x;
     int y;
-    int cnt;
     
-    Place(int x, int y, int cnt) {
+    Characters(int x, int y) {
         this.x = x;
         this.y = y;
-        this.cnt = cnt;
     }
 }
+/*
+0,0
+n-1, m-1
+*/
